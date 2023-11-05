bc726711a19a42673451faf1ff6891141843bafc
Henrry Beltrán <henrry26bt@gmail.com>
2023-11-04 20:51:57 -0500

    feat: Chapter 15 of Auth finished

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
 import "@/app/ui/global.css";
-import { geist } from "./ui/fonts";
+import { inter } from "./ui/fonts";
 
 type Props = { children: React.ReactNode };
 
 export default function RootLayout({ children }: Props) {
   return (
     <html lang="en">
-      <body className={`${geist.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,7 +1,7 @@
 "use server";
 
+import { signIn } from "@/auth";
 import { sql } from "@vercel/postgres";
-import { on } from "events";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { z } from "zod";
@@ -112,4 +112,18 @@ export async function deleteInvoice(id: string) {
   }
 
   revalidatePath("/dashboard/invoices");
+}
+
+export async function authenticate(
+  _prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn("credentials", Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes("CredentialsSignin")) {
+      return "CredentialSignin";
+    }
+    throw error;
+  }
 }
